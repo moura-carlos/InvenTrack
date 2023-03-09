22bb942fba36651142d049c9122ab2462af441d3
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-03-09 15:15:55 -0800

    Setting up User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_185639) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_225431) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_185639) do
   create_table "items", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
