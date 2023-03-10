9db1784d226cc6e2beeb0ab5a3ceb81ec62641e6
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-03-09 16:55:30 -0800

    Create CRUD actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,50 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, status: :see_other, notice: 'Account successfully created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, status: :see_other, notice: 'Account successfully updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to users_url, status: :see_other, notice: 'Account successfully deleted!'
+    else
+      render :show, notice: 'Something went wrong! Please, try again.'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
 end
