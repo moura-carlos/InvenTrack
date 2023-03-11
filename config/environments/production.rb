c435f561eeaba4be23f91cd5b1ec8ef2351b5fd3
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-03-10 19:45:25 -0800

    Create initial mailer version for notifying the user when a given item is out of stock.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp-relay.sendinblue.com',
+    port:                 587,
+    domain:               'inventrack.com',
+    user_name:            Rails.application.credentials[:sendinblue_username],
+    password:             Rails.application.credentials[:sendinblue_password],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 end
