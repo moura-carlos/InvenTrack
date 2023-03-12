783344bb266265303e9d07e5b0dfc6d6064f023c
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-03-12 14:54:47 -0700

    Categories Controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,55 @@
+class CategoriesController < ApplicationController
+  before_action :find_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, status: :see_other, notice: "Category was successfully created."
+    else
+      flash.now[:alert] = "Oops! Category could not be created."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, status: :see_other, notice: "Category was successfully updated."
+    else
+      flash.now[:alert] = "Oops! Category could not be updated."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      redirect_to categories_path, status: :see_other, notice: "Category was successfully deleted."
+    else
+      flash[:alert] = "Oops! Category could not be deleted."
+      redirect_to categories_path
+    end
+  end
+
+  private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
