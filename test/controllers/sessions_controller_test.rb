a1f82133438350e7612c0cc9b27ee47e62ba75de
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-03-14 13:38:19 -0700

    Add tests for SessionsController actions

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -4,4 +4,33 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get new" do
+    get new_session_url
+    assert_response :success
+  end
+
+  test "should create session on valid credentials" do
+    post session_url, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to user_path(@user)
+    assert_equal "Welcome back, #{@user.name}!", flash[:success]
+  end
+
+  test "should not create session on invalid credentials" do
+    post session_url, params: { session: { email: @user.email, password: 'wrong_password' } }
+    assert_response :unprocessable_entity
+    assert_equal "Invalid email or password", flash.now[:error]
+  end
+
+  test "should destroy session and log out" do
+    post session_url, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to user_path(@user)
+
+    delete session_url
+    assert_redirected_to root_path
+    assert_equal 'You have successfully logged out!', flash[:notice]
+  end
 end
