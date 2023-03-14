fced1b3a593ce2faadefa015511c66f527b53d08
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-03-14 14:26:21 -0700

    Test ItemMailer for out of stock email notifications

--- test/mailers/item_mailer_test.rb
+++ test/mailers/item_mailer_test.rb
@@ -1,12 +1,23 @@
 require "test_helper"
 
 class ItemMailerTest < ActionMailer::TestCase
+  # test "stock" do
+  #   mail = ItemMailer.stock
+  #   assert_equal "Stock", mail.subject
+  #   assert_equal ["to@example.org"], mail.to
+  #   assert_equal ["from@example.com"], mail.from
+  #   assert_match "Hi", mail.body.encoded
+  # end
   test "stock" do
-    mail = ItemMailer.stock
-    assert_equal "Stock", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
+    user = users(:one)
+    item = items(:one)
+    item.update(quantity: 0) # Ensure the item is out of stock
 
+    mail = ItemMailer.stock(item, user)
+    assert_equal "#{item.title} is out of stock!", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["carlos@example.com"], mail.from
+    assert_match "Dear #{user.name},", mail.body.encoded
+    assert_match "the inventory for your item, #{item.title}, is currently out of stock.", mail.body.encoded
+  end
 end
